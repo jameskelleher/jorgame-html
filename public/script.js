72f086405072fb250883cd2870cd855d34e142e8
James Kelleher <james.kelleher.3345@gmail.com>
2021-03-30 15:51:44 -0400

    good progress

--- public/script.js
+++ public/script.js
@@ -1,8 +1,10 @@
-// const placeholder = document.createElement("img");
-// placeholder.src = "assets/placeholder_card.png";
-// placeholder.classList.add("placeholder", "inSpotlight");
+// 146.78125
 
 const ANIM_DURATION = 800;
+const ANIM_PARAMS = {
+  duration: ANIM_DURATION,
+  easing: "ease-in-out"
+}
 
 const spotlightDiv = document.getElementById("spotlightDiv");
 const spotlightLeft = document.getElementById("spotlightLeft");
@@ -14,16 +16,6 @@ for (let card of document.getElementsByClassName("playerCard")) {
   moveOnClick(card);
 }
 
-function getSpotlightDest(element) {
-  if (spotlightCenter.children[0].classList.contains("placeholder")) {
-    return spotlightCenter;
-  } else if (element.classList.contains("playerCard")) {
-    return spotlightRight;
-  } else {
-    return spotlightLeft;
-  }
-}
-
 function moveOnClick(element) {
   element.onmousedown = moveElement;
 
@@ -31,75 +23,8 @@ function moveOnClick(element) {
 
     element.onmousedown = null;
 
-
-    const elemRect = element.getBoundingClientRect();
-
-    const destElem = getSpotlightDest(element);
-    const destRect = destElem.getBoundingClientRect();
-
-    const xTranslate = (destRect.x - elemRect.x) + (destRect.width / 2) - (elemRect.width / 2);
-    const yTranslate = (destRect.y - elemRect.y) + (destRect.height / 2) - (elemRect.height / 2);
-    const scale = destRect.height / elemRect.height;
-
-    var animation = element.animate({
-      transform: [
-        `translate(${xTranslate}px, ${yTranslate}px) scale(${scale})`,
-      ]
-    }, {
-      duration: ANIM_DURATION,
-      easing: "ease-in-out"
-    });
-    animation.onfinish = () => {
-      destElem.children[0].remove();
-      destElem.appendChild(element);
-      element.classList.remove("inDeck");
-      element.classList.add("inSpotlight");
-    }
-
-    const style = element.currentStyle || window.getComputedStyle(element);
-    const margin = parseInt(style.marginLeft.slice(0, -2));
-    const playedVal = element.id.split("_")[1]
-
-    for (let card of document.getElementsByClassName("inDeck")) {
-      if (card == element) {
-        continue;
-      }
-
-      const cardVal = card.id.split("_")[1];
-      var cardXTranslate = (elemRect.width / 2) + margin;
-
-      if (cardVal > playedVal) {
-        cardXTranslate *= -1;
-      }
-
-      card.animate({
-        transform: [
-          `translateX(${cardXTranslate}px)`,
-        ]
-      }, {
-        duration: ANIM_DURATION,
-        easing: "ease-in-out"
-      });
-    }
-
-    if (destElem !== spotlightCenter) {
-      const cardXTranslate = spotlightLeft.getBoundingClientRect().x - spotlightCenter.getBoundingClientRect().x;
-      const animation = spotlightCenter.children[0].animate({
-        transform: [
-          `translateX(${cardXTranslate}px)`,
-        ]
-      }, {
-        duration: ANIM_DURATION,
-        easing: "ease-in-out"
-      });
-
-      animation.onfinish = () => {
-        spotlightLeft.children[0].remove();
-        // const temp = spotlightLeft.children[0];
-        spotlightLeft.appendChild(spotlightCenter.children[0]);
-        // spotlightCenter.appendChild(temp);
-      }
-    }
+    animateToSpotlight(element);
+    animateCardsInDeck(element);
   }
 }
 
@@ -110,33 +35,104 @@ function buttonClicked() {
 
   const cardToMove = evenCards.item(evenCards.length - 1);
 
-  const destElem = getSpotlightDest(cardToMove);
+  animateToSpotlight(cardToMove);
+}
+
+function animate(element, destElem, transform, classAdd = "inSpotlight", classRemove = ["inDeck", "inOppDeck"]) {
+  animation = element.animate({
+    transform: transform
+  }, ANIM_PARAMS);
+
+  animation.onfinish = () => {
+    element.classList.remove(...classRemove);
+    element.classList.add(classAdd);
+    destElem.appendChild(element);
+  }
+}
+
+function animateToSpotlight(element) {
+  console.log(spotlightCenter.childElementCount);
+  if (spotlightCenter.childElementCount === 0) {
+    animateToSpotlightCenter(element);
+  } else if (element.classList.contains("playerCard")) {
+    animateToSpotlightRight(element);
+    animateToSpotlightLeft(spotlightCenter.children[0]);
+  } else {
+    animateToSpotlightLeft(element);
+    animateToSpotlightRight(spotlightCenter.children[0]);
+  }
+}
+
+function animateToSpotlightCenter(element) {
+  console.log("to spotlight center");
+  const elemRect = element.getBoundingClientRect();
+  const destElem = spotlightCenter;
   const destRect = destElem.getBoundingClientRect();
 
-  const ctmRect = cardToMove.getBoundingClientRect();
+  const xTranslate = (destRect.x - elemRect.x) + (destRect.width / 2) - (elemRect.width / 2);
+  const yTranslate = (destRect.y - elemRect.y) + (destRect.height / 2) - (elemRect.height / 2);
+  const scale = destRect.height / elemRect.height;
 
-  const xTranslate = (destRect.x - ctmRect.x) + (destRect.width / 2) - (ctmRect.width / 2);
-  const yTranslate = (destRect.y - ctmRect.y) + (destRect.height / 2) - (ctmRect.height / 2);
+  const transform = `translate(${xTranslate}px, ${yTranslate}px) scale(${scale})`;
 
-  const scale = destRect.height / ctmRect.height;
+  animate(element, destElem, transform);
+}
 
-  const translateAnim = cardToMove.animate({
-    transform: `translate(${xTranslate}px, ${yTranslate}px) scale(${scale}, ${scale})`,
-  }, {
-    duration: ANIM_DURATION,
-    easing: "ease-in-out",
-  });
+function animateToSpotlightRight(element) {
+  const elemRect = element.getBoundingClientRect();
+  const destElem = spotlightRight;
+  const destRect = destElem.getBoundingClientRect();
 
-  translateAnim.onfinish = () => {
-    destElem.children[0].remove();
-    destElem.appendChild(cardToMove);
-    cardToMove.classList.remove("inOppDeck");
-    cardToMove.classList.add("spotlight");
-  }
+  const scale = destRect.height / elemRect.height;
 
-  if (destElem !== spotlightCenter) {
-    const cardXTranslate = spotlightCenter.getBoundingClientRect().x - spotlightLeft.getBoundingClientRect().x;
-    const animation = spotlightCenter.children[0].animate({
+  const xCenter = elemRect.x + elemRect.width / 2;
+  const xAfterScale = xCenter - (elemRect.width / 2 * scale);
+  const xTranslate = destRect.x - xAfterScale - elemRect.width * scale;
+
+  const yTranslate = (destRect.y - elemRect.y) + (destRect.height / 2) - (elemRect.height / 2);
+
+  const transform = `translate(${xTranslate}px, ${yTranslate}px) scale(${scale})`;
+
+  animate(element, destElem, transform);
+}
+
+function animateToSpotlightLeft(element) {
+  const elemRect = element.getBoundingClientRect();
+  const destElem = spotlightLeft;
+  const destRect = destElem.getBoundingClientRect();
+
+  const scale = destRect.height / elemRect.height;
+
+  const xCenter = elemRect.x + elemRect.width / 2;
+  const xAfterScale = xCenter - (elemRect.width / 2 * scale);
+  const xTranslate = destRect.x - xAfterScale;
+
+  const yTranslate = (destRect.y - elemRect.y) + (destRect.height / 2) - (elemRect.height / 2);
+
+  const transform = `translate(${xTranslate}px, ${yTranslate}px) scale(${scale})`;
+
+  animate(element, destElem, transform);
+}
+
+function animateCardsInDeck(element) {
+  const style = element.currentStyle || window.getComputedStyle(element);
+  const margin = parseInt(style.marginLeft.slice(0, -2));
+  const playedVal = element.id.split("_")[1]
+
+  for (let card of document.getElementsByClassName("inDeck")) {
+    if (card == element) {
+      continue;
+    }
+
+    const cardVal = card.id.split("_")[1];
+    const cardWidth = card.getBoundingClientRect().width
+    var cardXTranslate = (cardWidth / 2) + margin;
+
+    if (cardVal > playedVal) {
+      cardXTranslate *= -1;
+    }
+
+    card.animate({
       transform: [
         `translateX(${cardXTranslate}px)`,
       ]
@@ -144,13 +140,5 @@ function buttonClicked() {
       duration: ANIM_DURATION,
       easing: "ease-in-out"
     });
-
-    animation.onfinish = () => {
-      const temp = spotlightRight.children[0];
-      spotlightRight.appendChild(spotlightCenter.children[0]);
-      spotlightCenter.appendChild(temp);
-    }
   }
-
-
 }
